134c96b5d38f6ba240a55034da1fc9397674e4b1
Miro Hrončok <miro@hroncok.cz>
2016-04-29 17:22:16 +0200

    Reindent some strings

--- utvsapi/auth.py
+++ utvsapi/auth.py
@@ -21,23 +21,23 @@ def preprocessor(cls, function_name, request):
         return
     token = headers_to_token(request.headers)
     if not token:
-        raise exceptions.UnauthorizedException('Token not provided. '
-                                               'Use the following header: '
-                                               'Authorization: Bearer {token}')
+        raise exceptions.UnauthorizedException(
+            'Token not provided. Use the following header: '
+            'Authorization: Bearer {token}')
     c = TokenClient(check_token_uri='http://localhost:8080/token',
                     usermap_uri='http://localhost:8080/user')
     info = None
     try:
         info = c.token_to_info(token)
     except:
-        raise exceptions.UnauthorizedException('Token not valid. '
-                                               'Please provide a valid token.')
+        raise exceptions.UnauthorizedException(
+            'Token not valid. Please provide a valid token.')
 
     # default behavior for all of our resources
     if 'cvut:utvs:general:read' not in info['scope']:
-        raise exceptions.ForbiddenException('Permission denied. '
-                                            'You need '
-                                            'cvut:utvs:general:read scope.')
+        raise exceptions.ForbiddenException(
+            'Permission denied. You need '
+            'cvut:utvs:general:read scope.')
 
     # add the information to the request, for further pre/postprocessors
     request.client_info = info
